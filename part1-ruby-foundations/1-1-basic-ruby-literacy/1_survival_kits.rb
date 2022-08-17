1c69748a8baf192eb3a366b06847e6896893b4e9
rikyhidayat21 <rikyhidayat21@gmail.com>
2022-08-18 00:04:25 +0700

    bismillah rubiyst!

---
+++ part1-ruby-foundations/1-1-basic-ruby-literacy/1_survival_kits.rb
@@ -0,0 +1,25 @@
+# 1.1.2 A ruby syntax survival kits
+# Ruby special objects begin
+# OPERATION Special value objects => true, false, nil
+# false and nil cause a conditional expression to evaluate as false;
+# all other objects (including true, 0, and empty strings) cause evaluate to *true*
+
+# DEFAULT OBJECT => self
+# keyword self refers to the default object
+# Methods calls that don't specify a calling object are called on *self* 
+# Ruby special objects end
+
+# 1.1.3 the variety of ruby identifier
+# Ruby identifiers begin
+# Variables: local, instance, class, global
+# constants, keywords, method names
+# local variables => use underscores, consist of letters, and or digits
+# instance variable => storing information within individual objects, always start with a single at-sign (@)
+# class variable => store information per class hierarchy, follow the same rules as instance variable, except start with double at-sign (@@)
+# global variable => using $
+# constants => begin with uppercase letter, FIRST_NAME
+# keywords => 
+# method name => follow the same rules and conventions as local variables (except that they can end with ?, !, or =).
+# methods don't call attention to themselves as methods but rather blend into the texture of a program as expression that provide a value.
+
+# Ruby identifiers end
