da8be1dbc4f6c57489503f398f038a9b421611a2
rikyhidayat21 <rikyhidayat21@gmail.com>
2022-09-02 05:53:12 +0700

    initialize exercise 5

---
+++ learn-the-hard-way/ex5.rb
@@ -0,0 +1,18 @@
+name = 'Riky Hidayat'
+age = 24 # 2022
+height = 169 # cm
+weight = 62 # kg
+eyes = 'Black'
+teeth = 'White'
+hair = 'Black'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} cm tall."
+puts "He's #{weight} kg heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{height}
+      I get #{age + height + weight}."
